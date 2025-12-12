59f398e1a075356401c843b784781727b6355341
Sokleap-SAM <leapngu2019@gmail.com>
2025-12-12 09:25:19 +0700

    first commit

---
+++ Task2/SharedData.java
@@ -0,0 +1,31 @@
+package Task2;
+
+public class SharedData {
+    private int currentNum = 1;
+    private int threadIndexTurn = 0;
+    private boolean isCountingUp = true;
+
+    public int getCurrentNum(){
+        return currentNum;
+    }
+
+    public int getCurrentThreadIndexTurn(){
+        return threadIndexTurn;
+    }
+
+    public void updateCurrentNum(int maxCount){
+        if (isCountingUp) {
+            this.currentNum++;
+            if (this.currentNum >= maxCount) {
+                this.currentNum = maxCount; 
+                this.isCountingUp = false;
+            }
+        } else {
+            this.currentNum--;
+        }
+    }
+
+    public void updateThreadIndexTurn(int newThreadIndexTurn){
+        this.threadIndexTurn = newThreadIndexTurn;
+    }
+}
